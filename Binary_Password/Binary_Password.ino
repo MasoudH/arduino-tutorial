bf5ee3969e2a539d70f016ff591c427534437811
MasoudH <masoudharati96@gmail.com>
2016-05-20 17:30:04 -0400

    Commenting for Binary Password

--- Binary_Password/Binary_Password.ino
+++ Binary_Password/Binary_Password.ino
@@ -1,14 +1,14 @@
-int B0State = 0;
-int B1State = 0;
+int B0State = 0; // State of Button 1
+int B1State = 0; // State of Button 2
 
-const String CorrectPassword = "01000100";
-String AttemptedPassword = "";
+const String CorrectPassword = "01000100"; // Correct Password
+String AttemptedPassword = ""; // String for users input
 
 bool IsButtonRegistered = false;
 
 void setup() {
-  pinMode(2, INPUT);
-  pinMode(4, INPUT);
+  pinMode(2, INPUT); // Input from button 1
+  pinMode(4, INPUT); // Input from button 2
 
   pinMode(12, OUTPUT);  // Correct
   pinMode(13, OUTPUT);  // Wrong
@@ -18,33 +18,41 @@ void setup() {
 
 void loop() {
   while (AttemptedPassword.length() < 8) {
-    B0State = digitalRead(2);
-    B1State = digitalRead(4);
+    B0State = digitalRead(2); // Read data from button 1
+    B1State = digitalRead(4); // Read data from button 2
     
     if (B0State == 0 && B1State == 0) {
       IsButtonRegistered = false;
     }
   
     if (B0State == 1 && !IsButtonRegistered){
+      // 0 input from button 1
+      
       AttemptedPassword += "0";
       IsButtonRegistered = true;
     }
     else if (B1State == 1 && !IsButtonRegistered) {
+      // 1 input from button 2
+      
       AttemptedPassword += "1";
       IsButtonRegistered = true;
     }
   }
 
   if (AttemptedPassword == CorrectPassword) {
+    // Correct Password - Green Light
+    
     digitalWrite(12, HIGH);
     digitalWrite(13, LOW);
   }
   else {
+    // Wrong Password - Red Light
+    
     digitalWrite(13, HIGH);
     digitalWrite(12, LOW);
   }
 
   delay(5000);
 
-  AttemptedPassword = "";
+  AttemptedPassword = ""; // Reset Users attempts
 }
--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Arduino Tutorial - MasseyHacks II
 
 This tutorial will occur in 3 steps
-1. Blinking Lights
-2. Binary Password
-3. Functioning Traffic Light
 
 ## Blinking Lights
 #### Breadboard Layout
@@ -38,7 +35,7 @@ void loop() {
 
 ## Binary Password
 #### Breadboard Layout
-![Blinking Light Breadboard Layout](http://github.com/MasoudH/arduino-tutorial/tree/master/Binary_Password/img/breadboard.JPG)
+![Binary Password Breadboard Layout](http://github.com/MasoudH/arduino-tutorial/tree/master/Binary_Password/img/breadboard.JPG)
 #### Code
 
 ## Functioning Traffic Light
